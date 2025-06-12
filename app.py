4228dcc0b26be5715b35ea4b5d9bb9b7d80de7d2
= <=>
2025-06-12 15:02:00 +0530

    added changes to deploy

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ eda.register_callbacks(app)
 forecast.register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
